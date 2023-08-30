aecd2293e7d403dd315fd1356f89a1ea6b123e35
asaini987 <135754860+asaini987@users.noreply.github.com>
2023-08-30 14:06:43 -0400

    Create EmojiArtApp.swift

---
+++ EmojiArtApp.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    @StateObject var document = EmojiArtDocument()
+    @StateObject var paletteStore = PaletteStore(named: "Default")
+    
+    var body: some Scene {
+        WindowGroup {
+            EmojiArtDocumentView(document: document)
+                .environmentObject(paletteStore)
+        }
+    }
+}
